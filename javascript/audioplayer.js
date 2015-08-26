663eb60a953a0a60c73780555fc7579f023cfc3e
YoanDieu <yoan.dieu@gmail.com>
2015-08-26 23:00:46 +0200

    having trouble with insertion as the last of the DOM, the element is here but does not appear. Set a play() fonction in button as a test, works

--- javascript/audioplayer.js
+++ javascript/audioplayer.js
@@ -1,17 +1,35 @@
 var $body = document.getElementsByTagName("body")[0];
 var fullPlayer = document.createElement("DIV");
 var audio = document.createElement("AUDIO");
+var source = document.createElement("SOURCE");
+var playBtn = document.createElement("BUTTON");
+var backwardBtn = document.createElement("DIV");
+var script = document.getElementsByTagName("script")[0];
+
 
 /*setting fullPlayer style */
 fullPlayer.style.backgroundColor = "black";
 fullPlayer.style.position = "fixed";
+fullPlayer.style.display = "block";
 fullPlayer.style.width = "100%";
 fullPlayer.style.height = "50px";
 
-/* settinf player attributs */
+/* setting player attributs */
 audio.id = "audio";
-audio.controls = "controls";
 
+/*setting source attributs*/
+source.src = "music/montageson.ogg";
+source.type = "audio/ogg";
+
+/* setting playerBtn attribut and style */
+playBtn.innerHTML = "play";
+
+
+
+audio.appendChild(source);
+fullPlayer.appendChild(playBtn);
 fullPlayer.appendChild(audio);
 
-$body.appendChild(fullPlayer);
+$body.insertBefore(fullPlayer, script);
+
+playBtn.addEventListener("click", function(e){ audio.play();}, false);
