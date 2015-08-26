79467f6ba50020775c1a8e68e9992b02a07fa6d5
YoanDieu <yoan.dieu@gmail.com>
2015-08-26 20:20:28 +0200

    added audio balise to player

--- javascript/audioplayer.js
+++ javascript/audioplayer.js
@@ -1,10 +1,17 @@
-var fullPlayer = document.createElement("DIV");
 var $body = document.getElementsByTagName("body")[0];
+var fullPlayer = document.createElement("DIV");
+var audio = document.createElement("AUDIO");
 
+/*setting fullPlayer style */
 fullPlayer.style.backgroundColor = "black";
 fullPlayer.style.position = "fixed";
 fullPlayer.style.width = "100%";
 fullPlayer.style.height = "50px";
 
+/* settinf player attributs */
+audio.id = "audio";
+audio.controls = "controls";
+
+fullPlayer.appendChild(audio);
 
 $body.appendChild(fullPlayer);
