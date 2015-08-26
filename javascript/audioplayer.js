acdffb23e5b70d29c28931793e7525ed2aeed97f
YoanDieu <yoan.dieu@gmail.com>
2015-08-26 19:37:08 +0200

    created the audioplayer container and inserted it in the dom as a test

--- javascript/audioplayer.js
+++ javascript/audioplayer.js
@@ -1,1 +1,10 @@
-alert("test !");
+var fullPlayer = document.createElement("DIV");
+var $body = document.getElementsByTagName("body")[0];
+
+fullPlayer.style.backgroundColor = "black";
+fullPlayer.style.position = "fixed";
+fullPlayer.style.width = "100%";
+fullPlayer.style.height = "50px";
+
+
+$body.appendChild(fullPlayer);
